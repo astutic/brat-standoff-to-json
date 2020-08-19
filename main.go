192e5cad0465a75f29ab74282b9871cba832bf23
Nithin Stephen <nithin.stephen@astutic.com>
2020-08-19 16:05:43 +0530

    AT-258 AT-163 implemented brat standoff to Acharya converter

---
+++ main.go
@@ -0,0 +1,308 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+type AcharyaStruct struct {
+	Data     string
+	Entities map[int]AcharyaEntity
+}
+
+type AcharyaEntity struct {
+	begin int
+	end   int
+	name  string
+}
+
+func getSubString(originalString string, startPos, endPos int) string {
+	counter := 0
+	val := ""
+	var r rune
+	for i, s := 0, 0; i <= len(originalString); i += s {
+		r, s = utf8.DecodeRuneInString(originalString[i:])
+		if r == '\r' {
+			continue
+		}
+		if counter >= startPos {
+			if counter >= endPos {
+				break
+			}
+			val = val + string(r)
+		}
+		counter++
+	}
+	return val
+}
+
+func getEntities(confFile *os.File) map[string]bool {
+
+	scannerC := bufio.NewScanner(confFile)
+	scannerC.Split(bufio.ScanLines)
+	startScan := false
+	entities := make(map[string]bool)
+
+	for scannerC.Scan() {
+		if strings.Contains(scannerC.Text(), "[entities]") {
+			startScan = true
+			continue
+		}
+		if startScan == true {
+			if len(strings.TrimSpace(scannerC.Text())) == 0 {
+				continue
+			}
+			if strings.HasPrefix(scannerC.Text(), "[") {
+				break
+			} else if strings.HasPrefix(scannerC.Text(), "#") {
+				continue
+			}
+			entities[strings.TrimSpace(scannerC.Text())] = true
+		}
+	}
+	if len(entities) == 0 {
+		log.Fatal("The conf file does not have an `[entities]` field or `[entities]` field is empty")
+	}
+	return entities
+}
+
+func generatePaths(path string) ([]string, []string, error) {
+	confCount := 0
+	annMult := []string{}
+	textMult := []string{}
+	err := filepath.Walk(path,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if confCount > 1 {
+				log.Fatalf("Multiple `annotation.conf` files found")
+			}
+			if strings.HasSuffix(path, ".ann") {
+				if _, err := os.Stat(strings.TrimSuffix(path, ".ann") + ".txt"); os.IsNotExist(err) {
+					log.Fatalf("%s File does not exist", strings.TrimSuffix(path, ".ann")+".txt")
+				}
+				annMult = append(annMult, path)
+				textMult = append(textMult, strings.TrimSuffix(path, ".ann")+".txt")
+			} else if strings.HasSuffix(path, ".txt") {
+				if _, err := os.Stat(strings.TrimSuffix(path, ".txt") + ".ann"); os.IsNotExist(err) {
+					log.Fatalf("%s File does not exist", strings.TrimSuffix(path, ".txt")+".ann")
+				}
+			} else if strings.HasSuffix(path, "annotation.conf") {
+				confCount++
+			}
+			return nil
+		})
+	if err != nil {
+		return []string{}, []string{}, err
+	}
+	return annMult, textMult, nil
+}
+
+func generateEntityMap(ent map[string]bool, aData *os.File) map[int]AcharyaEntity {
+	scanner := bufio.NewScanner(aData)
+	scanner.Split(bufio.ScanLines)
+
+	entityMap := make(map[int]AcharyaEntity)
+	count := 0
+
+	for scanner.Scan() {
+		// Uncomment the lines below to dispaly the ann file
+		// fmt.Println(strings.Repeat("#", 30), "Annotations", strings.Repeat("#", 30))
+		// fmt.Println(scanner.Text())
+		if strings.HasPrefix(scanner.Text(), "T") {
+			splitAnn := strings.Split(strings.Replace(scanner.Text(), "\t", ",", 2), ",")
+			if strings.Contains(splitAnn[1], ";") {
+				panic("discontinuous text-bound annotations is not currently supported")
+			}
+			entAndPos := strings.Split(splitAnn[1], " ")
+			if ent[strings.TrimSpace(entAndPos[0])] {
+				b, _ := strconv.Atoi(entAndPos[1])
+				e, _ := strconv.Atoi(entAndPos[2])
+				entityMap[count] = AcharyaEntity{b, e, entAndPos[0]}
+			}
+			count++
+		}
+	}
+
+	return entityMap
+}
+
+func generateAcharyaAndConvert(tData string, ent map[string]bool, entMap map[int]AcharyaEntity) (string, string) {
+	conv := ""
+	acharya := fmt.Sprintf("{\"Data\":\"%s\",\"Entities\":[", tData)
+	j := 0
+	c := 0
+	for {
+		ent, ok := entMap[j]
+		if ok {
+			conv = conv + fmt.Sprintf("T%d\t%s %d %d\t%s\n", j+1, ent.name, ent.begin, ent.end, getSubString(tData, ent.begin, ent.end))
+			acharya = acharya + fmt.Sprintf("[%d,%d,\"%s\"],", ent.begin, ent.end, ent.name)
+			c++
+		}
+		// loop until all entities in a are fetched
+		if c >= len(entMap) {
+			conv = strings.TrimSuffix(conv, "\n")
+			acharya = strings.TrimSuffix(acharya, ",")
+			acharya = strings.ReplaceAll(acharya, "\n", "\\n")
+			acharya = acharya + "]}\n"
+			break
+		}
+		j++
+	}
+	return acharya, conv
+}
+
+func handleOutput(outputFile, acharya, textfileName string, overWrite bool, currentParseIndex int) {
+	if outputFile == "" {
+		fmt.Println(acharya)
+	} else {
+
+		// asking for the first time
+		if currentParseIndex == 0 && !overWrite {
+			if _, err := os.Stat(fmt.Sprintf("%s", outputFile)); !os.IsNotExist(err) {
+				log.Fatalf("The output file already exists use `--overwrite flag to overwrite the file`")
+			}
+		}
+
+		if currentParseIndex == 0 && overWrite {
+			if _, err := os.Stat(fmt.Sprintf("%s", outputFile)); !os.IsNotExist(err) {
+				e := os.Remove(outputFile)
+				if e != nil {
+					log.Fatal(e)
+				}
+			}
+
+		}
+
+		f, err := os.OpenFile(fmt.Sprintf("%s", outputFile), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			panic(err)
+		}
+		if _, err = f.WriteString(acharya); err != nil {
+			panic(err)
+		}
+		if currentParseIndex == 0 {
+			fmt.Printf("Successfully generated `%s` for %s\n", outputFile, strings.TrimSpace(textfileName))
+		} else {
+			fmt.Printf("Successfully updated `%s` for %s\n", outputFile, strings.TrimSpace(textfileName))
+		}
+	}
+}
+
+func handleMain(all, conf, opfile, anns, texts string, overwrite bool) {
+	annMult := []string{}
+	textMult := []string{}
+	var err error
+	if all != "" {
+		annMult, textMult, err = generatePaths(all)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	var confPath string
+
+	if all != "" {
+		confPath = all + "/annotation.conf"
+	} else {
+		confPath = conf
+	}
+
+	cDat, cErr := os.Open(confPath)
+	if cErr != nil {
+		log.Fatal(cErr)
+	}
+	defer cDat.Close()
+
+	ent := getEntities(cDat)
+
+	if all == "" {
+		annMult = strings.Split(anns, ",")
+		textMult = strings.Split(texts, ",")
+	}
+
+	if len(annMult) != len(textMult) {
+		log.Fatal("The number of annotation files should be equal to the number of text files")
+	}
+
+	for i := range annMult {
+		aData, aErr := os.Open(strings.TrimSpace(annMult[i]))
+		if aErr != nil {
+			log.Fatal(aErr)
+		}
+		defer aData.Close()
+
+		tDat, tErr := os.Open(strings.TrimSpace(textMult[i]))
+		if tErr != nil {
+			log.Fatal(tErr)
+		}
+		defer tDat.Close()
+
+		tData := ""
+		scannerD := bufio.NewScanner(tDat)
+		scannerD.Split(bufio.ScanLines)
+
+		for scannerD.Scan() {
+			// add `\n` since they are removed by `scannerD.Split(bufio.ScanLines)`
+			tData = tData + scannerD.Text() + "\n"
+		}
+
+		a := AcharyaStruct{}
+
+		a.Entities = generateEntityMap(ent, aData)
+		a.Data = string(tData)
+		acharya, _ := generateAcharyaAndConvert(a.Data, ent, a.Entities)
+
+		handleOutput(opfile, acharya, textMult[i], overwrite, i)
+
+		// Uncomment the below lines to show the converted files
+		// fmt.Println(strings.Repeat("#", 30), "Convert", strings.Repeat("#", 30))
+		// fmt.Println(conv)
+
+		// uncomment the  below lines to write an output.ann file
+		// err := ioutil.WriteFile("./output.ann", []byte(conv), 0644)
+		// if err != nil {
+		// 	panic(err)
+		// }
+
+		// fmt.Println("Successfully `output.ann` in the current diretory")
+	}
+}
+
+func main() {
+	fmt.Println("Starting Acharya service ...")
+	all := flag.String("all", "", "Path to your folder")
+	anns := flag.String("ann", "", "Comma sepeartad locations of the annotation files (ann)")
+	texts := flag.String("text", "", "Comma sepeartad locations of the text files (txt)")
+	conf := flag.String("conf", "", "Location of the annotation configuration file (.conf)")
+	oFileName := flag.String("output", "", "Name for the output file")
+	overWrite := flag.Bool("overwrite", false, "If you wish to overwrite the generated file then set overwrite to true")
+
+	flag.Parse()
+
+	if *all == "" {
+		flag.VisitAll(func(f *flag.Flag) {
+			if f.Value.String() == "" {
+				if (f.Name != "output") && (f.Name != "all") {
+					log.Fatalf("kindly set the value for -%s or --%s\n", f.Name, f.Name)
+				}
+			}
+		})
+	}
+
+	if *overWrite && *oFileName == "" {
+		log.Fatalf("overwrite flag is provided but output file is not specified")
+	}
+
+	handleMain(*all, *conf, *oFileName, *anns, *texts, *overWrite)
+
+}
---
+++ main_test.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func TestGetSubString(t *testing.T) {
+	type params struct {
+		originalString string
+		startPos       int
+		endPos         int
+	}
+
+	tests := []struct {
+		input    *params
+		expected string
+	}{
+		{&params{"Hello world", 0, 2}, "He"},
+		{&params{"Apple MacBook Pro (16-inch", 0, 2}, "Ap"},
+		{&params{"नमस्ते दुनिया", 0, 2}, "नम"},
+		{&params{"ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ", 0, 2}, "ਸਤ"},
+		{&params{"ഹലോ വേൾഡ്", 0, 2}, "ഹല"},
+		{&params{"こんにちは世界", 0, 2}, "こん"},
+	}
+	for _, testCase := range tests {
+		output := getSubString(testCase.input.originalString, testCase.input.startPos, testCase.input.endPos)
+		if output != testCase.expected {
+			log.Errorf("Test FAILED: input: %+v, expected: %s, received: %s", testCase.input, testCase.expected, output)
+		}
+	}
+}
+
+func TestGetEntities(t *testing.T) {
+
+	confPaths := []string{
+		"./testData/news/annotation.conf",
+		"./testData/CoNLL-ST_2002/annotation.conf",
+	}
+
+	entities := [][]string{
+		[]string{"Person", "Organization", "GPE", "Money", "Inv1"},
+		[]string{"ORG", "PER", "LOC", "MISC", "Inv2"},
+	}
+	invalid := []string{"Inv1", "Inv2"}
+
+	for i, v := range entities {
+		cDat, cErr := os.Open(confPaths[i])
+		if cErr != nil {
+			log.Fatal(cErr)
+		}
+		defer cDat.Close()
+		output := getEntities(cDat)
+		for _, e := range v {
+			_, ok := output[e]
+			if !ok {
+				if e != invalid[0] && e != invalid[1] {
+					log.Errorf("Test FAILED: %s was not found in the annotation file at %s", e, confPaths[i])
+				}
+			}
+		}
+	}
+}
+
+func TestGeneratePaths(t *testing.T) {
+
+	conll2002Ann := []string{"testData/CoNLL-ST_2002/esp/esp.train-doc-100.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-1400.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-423.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-46.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-503.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-896.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-919.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-978.ann", "testData/CoNLL-ST_2002/esp/esp.train-doc-989.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-118.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-123.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-134.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-181.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-184.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-236.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-251.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-27.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-46.ann", "testData/CoNLL-ST_2002/ned/ned.train-doc-75.ann"}
+	conll2002Txt := []string{"testData/CoNLL-ST_2002/esp/esp.train-doc-100.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-1400.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-423.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-46.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-503.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-896.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-919.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-978.txt", "testData/CoNLL-ST_2002/esp/esp.train-doc-989.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-118.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-123.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-134.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-181.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-184.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-236.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-251.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-27.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-46.txt", "testData/CoNLL-ST_2002/ned/ned.train-doc-75.txt"}
+
+	anns, txts, _ := generatePaths("./testData/CoNLL-ST_2002")
+
+	for i, v := range conll2002Ann {
+
+		if v != filepath.ToSlash(anns[i]) {
+			log.Errorf("Test FAILED: Annotation file path %s is not equal to %s", strings.Replace(anns[i], "/", "\\", -1), v)
+		}
+		if conll2002Txt[i] != filepath.ToSlash(txts[i]) {
+			log.Errorf("Test FAILED: Text file path %s is not equal to %s", strings.Replace(txts[i], "/", "\\", -1), v)
+		}
+
+	}
+
+}
+
+func TestGenerateEntityMap(t *testing.T) {
+
+	expected := []AcharyaEntity{AcharyaEntity{418, 426, "Organization"},
+		AcharyaEntity{456, 468, "Money"},
+		AcharyaEntity{0, 0, ""},
+		AcharyaEntity{473, 496, "Person"},
+		AcharyaEntity{511, 535, "Person"},
+		AcharyaEntity{540, 545, "Organization"},
+		AcharyaEntity{549, 560, "GPE"},
+	}
+
+	cDat, cErr := os.Open("./testData/news/annotation.conf")
+	if cErr != nil {
+		log.Fatal(cErr)
+	}
+	defer cDat.Close()
+	aDat, aErr := os.Open("./testData/news/000-introduction.ann")
+	if aErr != nil {
+		log.Fatal(aErr)
+	}
+	defer aDat.Close()
+	ent := getEntities(cDat)
+	entMap := generateEntityMap(ent, aDat)
+
+	for i, v := range expected {
+		if entMap[i] != v {
+			log.Errorf("Test FAILED: entity at postion %d does not match (%+v != %+v) ", i, entMap[i], v)
+		}
+	}
+
+}
+
+func TestGenerateAcharyaAndConvert(t *testing.T) {
+
+	expected := `{"Data":"Welcome to the Brat Rapid Annotation Tool (brat) tutorial!\n\nbrat is a web-based tool for structured text annotation and visualization. The easiest way to explain what this means is by example: see the following sentence illustrating various types of annotation. Take a moment to study this example, moving your mouse cursor over some of the annotations. Hold the cursor still over an annotation for more detail.\n\n\n1 ) Citibank was involved in moving about $100 million for Raul Salinas de Gortari, brother of a former Mexican president, to banks in Switzerland.\n\n\nIf this example seems complicated, don't panic! This tutorial will present the key features of brat interactively, with each document presenting one or a few features. If you follow this brief tutorial, you'll be able to understand and create annotations such as those above in no time.\n\nTry moving to the next document now by clicking on the arrow to the right on the blue bar at the top left corner of the page.\n","Entities":[[418,426,"Organization"],[456,468,"Money"],[473,496,"Person"],[511,535,"Person"],[540,545,"Organization"],[549,560,"GPE"]]}` + "\n"
+	cDat, cErr := os.Open("./testData/news/annotation.conf")
+	if cErr != nil {
+		log.Fatal(cErr)
+	}
+	defer cDat.Close()
+	aDat, aErr := os.Open("./testData/news/000-introduction.ann")
+	if aErr != nil {
+		log.Fatal(aErr)
+	}
+	defer aDat.Close()
+
+	tDat, tErr := os.Open("./testData/news/000-introduction.txt")
+	if tErr != nil {
+		log.Fatal(tErr)
+	}
+	defer tDat.Close()
+
+	tData := ""
+	scannerD := bufio.NewScanner(tDat)
+	scannerD.Split(bufio.ScanLines)
+
+	for scannerD.Scan() {
+		tData = tData + scannerD.Text() + "\n"
+	}
+
+	ent := getEntities(cDat)
+	entMap := generateEntityMap(ent, aDat)
+	acharya, _ := generateAcharyaAndConvert(string(tData), ent, entMap)
+
+	if acharya != expected {
+		log.Errorf("Test FAILED: generated acharya format does not match with expected.\n Generated:%s \n\n\n Expected:%s ", acharya, expected)
+	}
+
+}
+
+func TestHandleOutput(t *testing.T) {
+	e := os.Remove("./testData/file_gen_by_handleOutputTest.jsonl")
+	if e != nil {
+		log.Fatal(e)
+	}
+	acharya := `{"Data":"Welcome to the Brat Rapid Annotation Tool (brat) tutorial!\n\nbrat is a web-based tool for structured text annotation and visualization. The easiest way to explain what this means is by example: see the following sentence illustrating various types of annotation. Take a moment to study this example, moving your mouse cursor over some of the annotations. Hold the cursor still over an annotation for more detail.\n\n\n1 ) Citibank was involved in moving about $100 million for Raul Salinas de Gortari, brother of a former Mexican president, to banks in Switzerland.\n\n\nIf this example seems complicated, don't panic! This tutorial will present the key features of brat interactively, with each document presenting one or a few features. If you follow this brief tutorial, you'll be able to understand and create annotations such as those above in no time.\n\nTry moving to the next document now by clicking on the arrow to the right on the blue bar at the top left corner of the page.\n","Entities":[[418,426,"Organization"],[456,468,"Money"],[473,496,"Person"],[511,535,"Person"],[540,545,"Organization"],[549,560,"GPE"]]}` + "\n"
+	handleOutput("./testData/file_gen_by_handleOutputTest.jsonl", acharya, "not loaded txt", false, 0)
+	achDat, achErr := os.Open("./testData/file_gen_by_handleOutputTest.jsonl")
+	if achErr != nil {
+		log.Fatal(achErr)
+	}
+
+	achData := ""
+	scannerD := bufio.NewScanner(achDat)
+	scannerD.Split(bufio.ScanLines)
+
+	for scannerD.Scan() {
+		achData = achData + scannerD.Text() + "\n"
+	}
+
+	if achData != acharya {
+		log.Errorf("Test FAILED: content in the file ./testData/test.jsonl do not match with Expected:%s Got:%s", acharya, achData)
+	}
+
+}
+
+func TestHandleMain(t *testing.T) {
+
+	handleMain("./testData/news", "", "./testData/file_gen_by_handleMainTest.jsonl", "", "", true)
+
+	genDat, genErr := os.Open("./testData/file_gen_by_handleMainTest.jsonl")
+	if genErr != nil {
+		log.Fatal(genErr)
+	}
+
+	genData := ""
+	scanner := bufio.NewScanner(genDat)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		genData = genData + scanner.Text() + "\n"
+	}
+
+	cmpDat, cmpErr := os.Open("./testData/test_cmp.jsonl")
+	if cmpErr != nil {
+		log.Fatal(genErr)
+	}
+
+	cmpData := ""
+	scanner = bufio.NewScanner(cmpDat)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		cmpData = cmpData + scanner.Text() + "\n"
+	}
+
+	if genData != cmpData {
+		log.Errorf("Test FAILED: The generated file and the reference file don't match \n\n Expected: %s \n\n Got: %s", cmpData, genData)
+	}
+
+}
